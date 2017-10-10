9670e63edad860fc4c7ae07d2e1112a8f49fae46
Pihla Toivanen <toivanenpihla@gmail.com>
2017-10-10 15:55:57 +0300

    Shape directory to geo files

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-world = gpd.read_file('../data/citydata/geo_export.shp')
+world = gpd.read_file('../data/zip/geo_export.shp')
+boundaries = gpd.read_file('../data/zip/geo_export.shp')
 
 #cities.plot(marker='*', color='green', markersize=5)
 #cities = cities.to_crs(world.crs)
 
 base = world.plot(color='white', edgecolor='black')
-#cities.plot(ax=base, marker='o', color='red', markersize=5)
+boundaries.plot(ax=base)
 
 plt.show()
